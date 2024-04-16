0d12d2ceee83a45890c40a6aa97c4a75b1f73771
José Pedro Sousa <github@zpedro.dev>
2024-04-16 12:14:45 +0100

    test

---
+++ .devcontainer/onCreateCommand.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+curl -s install.aztec.network | NON_INTERACTIVE=1 BIN_PATH=/usr/local/bin bash -s
+docker compose -f $HOME/.aztec/docker-compose.yml pull
+
+echo 'y' | npx create-aztec-app -t app -n react -s
+cd react
+yarn
+
+npx -y playwright@1.42 install --with-deps
+yarn prep
