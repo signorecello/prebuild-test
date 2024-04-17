2166db05abe856a5ec1591ceb64ff7d1b82954e9
José Pedro Sousa <outgoing@zpedro.dev>
2024-04-17 19:29:49 +0100

    test

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -3,6 +3,10 @@
 curl -s install.aztec.network | NON_INTERACTIVE=1 BIN_PATH=/usr/local/bin bash -s
 docker compose -f $HOME/.aztec/docker-compose.yml pull
 
+if ! grep -q "PXE_URL" ~/.bashrc; then
+    echo "export PXE_URL=https://\$CODESPACE_NAME-8080.preview.\$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN" >> ~/.bashrc
+fi
+
 echo 'y' | npx create-aztec-app -t app -n react -s
 cd react
 yarn
--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
+apt install gh
 gh codespace ports visibility 8080:public -c $CODESPACE_NAME
-export PXE_URL="https://$CODESPACE_NAME-8080.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"
+
 (nohup /usr/local/bin/aztec sandbox &)
-cd react
